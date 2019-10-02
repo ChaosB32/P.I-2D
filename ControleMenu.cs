ed6645f499eb36792ec94b98b9a153557f79d8f5
ChaosB32 <54641909+ChaosB32@users.noreply.github.com>
2019-10-02 14:55:34 -0300

    Add files via upload

---
+++ ControleMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControleMenu : MonoBehaviour {
+
+	public void ButtonStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ButtonOptions()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void ButtonQuit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Vida.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vida : MonoBehaviour {
+
+    public int vida;
+    public int numVidas;
+
+    public Image[] caveiras;
+    public Sprite caveiraInteira;
+    public Sprite caveiraMetade;
+    public Sprite caveiraVazia;
+    
+    void Update()
+    {
+        if(vida > numVidas)
+        {
+            vida = numVidas;
+        }
+        for (int i = 0; i < caveiras.Length; i++)
+        {
+            if (i < vida)
+            {
+                caveiras[i].sprite = caveiraInteira;
+            }
+            else
+            {
+                caveiras[i].sprite = caveiraVazia;
+            }
+            if(i < numVidas)
+            {
+                caveiras[i].enabled = true;
+            }
+            else
+            {
+                caveiras[i].enabled = false;
+            }
+
+        }
+    }
+}
