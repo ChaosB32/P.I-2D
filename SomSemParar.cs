f4a8d62de42997c5a3982b3601dd62fe28ba9286
ChaosB32 <54641909+ChaosB32@users.noreply.github.com>
2019-10-17 11:35:57 -0300

    Add files via upload

---
+++ SomSemParar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomSemParar : MonoBehaviour {
+
+    public static SomSemParar instance;
+
+    void Awake()
+    {
+        if (SomSemParar.instance == null)
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            SomSemParar.instance = this;
+        }else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
